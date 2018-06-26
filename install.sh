de7c92a667c537dd3d97488f37262c8e053085c1
mzanella <mzanella@users.noreply.github.com>
2018-06-26 13:21:31 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 echo "----=====STARTING CUSTOM INSTALLATION=====----"
 
+echo "Rep" > prova.txt
+
 sudo gpg --keyserver hkp://keys.gnupg.net --recv-key C7E1F2D1B8A6BCA36A46F033EF11C639332995F2
 sudo apt-get --allow-unauthenticated update && sudo apt-get install --allow-unauthenticated -y iperf screen
 
